a0fcb974c731506619a5194920414269fe7101f3
jberry93 <jeffreyberry93@gmail.com>
2020-04-30 11:34:46 -0700

    feat(init): initialize repo

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as styles from './index.less';
+
+export default (): JSX.Element => (
+    <div>
+        <h1 className={styles.title}>Page Index</h1>
+    </div>
+);
